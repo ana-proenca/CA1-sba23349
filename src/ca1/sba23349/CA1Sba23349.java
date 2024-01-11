d28d1e8cb6b8cca84049f6b3fadc4aee0b5afa17
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2024-01-11 14:23:53 +0000

    Reading File working

--- src/ca1/sba23349/CA1Sba23349.java
+++ src/ca1/sba23349/CA1Sba23349.java
@@ -4,17 +4,32 @@
  */
 package ca1.sba23349;
 
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 /**
  *
  * @author anaclaudiaproenca
  */
 public class CA1Sba23349 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        try {
+            // Reading the data from the file.
+            Scanner sc = new Scanner(new FileReader("test/student.txt"));
+            
+            
+            while (sc.hasNextLine()) {
+                String fileData = sc.nextLine();
+                System.out.println(fileData);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
-    
 }
