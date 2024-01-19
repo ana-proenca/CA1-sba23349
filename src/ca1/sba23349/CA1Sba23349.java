922d4a9ebb9e93a4d49e516d53446d8b6f4baefb
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2024-01-19 18:57:30 +0000

    BufferdWriter and a method for Number of Classes

--- src/ca1/sba23349/CA1Sba23349.java
+++ src/ca1/sba23349/CA1Sba23349.java
@@ -7,6 +7,7 @@ package ca1.sba23349;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -19,25 +20,71 @@ public class CA1Sba23349 {
 
         String inputFile = "student.txt";
         String outputFile = "status.txt";
-        
+
         String[] dataFile = new String[3];
 
         try {
             Scanner sc = new Scanner(new FileReader(inputFile));
-            
+
             int index = 0;
             while (sc.hasNextLine()) {
                 dataFile[index] = sc.nextLine();
                 index++;
             }
-           
+
             System.out.println(dataFile[0]);
             System.out.println(dataFile[1]);
             System.out.println(dataFile[2]);
 
-        } catch (Exception e) {
+            BufferedWriter br = new BufferedWriter(new FileWriter(outputFile));
+
+            int i = 0;
+            while (i < dataFile.length) {
+                br.write(dataFile[i]);
+                br.newLine();
+                i++;
+            }
+            br.close();
+
+        } catch (IOException e) {
             System.out.println(e);
         }
 
+        String fullName = dataFile[0];
+
+        if (!fullName.contains(" ")) {
+            System.out.println("First and second names need to be separated by a single space.");
+        }
+        
+        String firstName = fullName.substring(0, fullName.indexOf(" "));
+        System.out.println(firstName);
+
+        if (!firstName.matches("[A-Za-z]+")) {
+            System.out.println("First name must be letters only");
+        }
+        if (!fullName.contains(" ")) {
+            System.out.println("First and second names need to be separated by a single space.");
+        } else {
+            String secondName = fullName.substring(fullName.indexOf(" ")+1);
+            System.out.println(secondName);
+        }
+
+    int numberClasses = Integer.parseInt(dataFile[1]);
+    String resultWorkload = workload(numberClasses);
+    System.out.println(resultWorkload);
+
+    }
+    public static String workload(int numberClasses) {
+        if (numberClasses == 1) {
+            return "Very Light";
+        } else if (numberClasses == 2) {
+            return "Light";
+        } else if (numberClasses >= 3 || numberClasses <= 5 ) {
+            return "Part Time";
+        } else if (numberClasses <=  0 || numberClasses > 8) {
+            return "Enter a valid number of classes";
+        } else {
+            return "Full Time";
+        }
     }
 }
