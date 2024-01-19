527f2da12feb90d403cf896ecb69bb2ff00c4387
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2024-01-19 19:35:11 +0000

    method validating Student Name and Number of Classes

--- src/ca1/sba23349/CA1Sba23349.java
+++ src/ca1/sba23349/CA1Sba23349.java
@@ -50,41 +50,70 @@ public class CA1Sba23349 {
             System.out.println(e);
         }
 
-        String fullName = dataFile[0];
-
-        if (!fullName.contains(" ")) {
-            System.out.println("First and second names need to be separated by a single space.");
-        }
-        
-        String firstName = fullName.substring(0, fullName.indexOf(" "));
-        System.out.println(firstName);
-
-        if (!firstName.matches("[A-Za-z]+")) {
-            System.out.println("First name must be letters only");
-        }
-        if (!fullName.contains(" ")) {
-            System.out.println("First and second names need to be separated by a single space.");
-        } else {
-            String secondName = fullName.substring(fullName.indexOf(" ")+1);
-            System.out.println(secondName);
-        }
-
+    String fullName = dataFile[0];
+    boolean isStudentNameValid = studentNameValidation(fullName);
+    
     int numberClasses = Integer.parseInt(dataFile[1]);
+    
+    boolean isNumberClassesValid = workloadValidation(numberClasses);
+
+        if (isStudentNameValid && isNumberClassesValid) {
+            System.out.println("Student Name and Number of Classes are valid");
+        } else {
+            System.out.println("Student Name and Number of Classes are  NOT valid");
+        }
+
+
+
+
+    
     String resultWorkload = workload(numberClasses);
     System.out.println(resultWorkload);
 
+    String studentNumber = dataFile[2];
     }
+
+    public static boolean studentNameValidation(String fullName) {
+        if (!fullName.contains(" ")) {
+            System.out.println("First and second names need to be separated by a single space.");
+            return false;
+        } 
+        String firstName = fullName.substring(0, fullName.indexOf(" "));
+        System.out.println(firstName);
+    
+        if (!firstName.matches("[A-Za-z]+")) {
+            System.out.println("First name must be letters only");
+            return false;
+        }
+        if (!fullName.contains(" ")) {
+            System.out.println("First and second names need to be separated by a single space.");
+            return false;
+        } else {
+            String secondName = fullName.substring(fullName.indexOf(" ")+1);
+            System.out.println(secondName);
+            return true;
+        }
+    }
+
     public static String workload(int numberClasses) {
         if (numberClasses == 1) {
             return "Very Light";
         } else if (numberClasses == 2) {
             return "Light";
-        } else if (numberClasses >= 3 || numberClasses <= 5 ) {
+        } else if (numberClasses >= 3 && numberClasses <= 5 ) {
             return "Part Time";
         } else if (numberClasses <=  0 || numberClasses > 8) {
-            return "Enter a valid number of classes";
+            return "Enter a valid number of classes: between 1 and 8";
         } else {
             return "Full Time";
         }
+    }
+
+    public static boolean workloadValidation(int numberClasses) {
+        return numberClasses >  0 && numberClasses <= 8;
+    }
+
+    public static void studentNumberChecker(String studentNumber) {
+
     }
 }
